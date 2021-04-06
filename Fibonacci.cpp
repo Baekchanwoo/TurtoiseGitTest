31ef4b080ff9a4ab8d7613eb26dd100c93948ab7
ChanwooBaek <bcw043785@gmail.com>
2021-04-06 11:57:43 +0900

    알고리즘 변경으로 코드의 비효율성 개선

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
-int RecursionFibonachi(int n) {
-	if (n == 0 || n == 1)
-		return n;
-	else
-		return RecursionFibonachi(n - 2) + RecursionFibonachi(n - 1);
+int IterationFibonachi(int n) {
+	int* array = (int*)malloc(sizeof(int) * n);
+
+	for (int j = 0; j < n; j++) {
+		array[j] = 0;
+	}
+
+	for (int i = 0; i < n+1; i++) {
+		if (i == 0 || i == 1) {
+			array[i] = i;
+		}
+		else{
+			array[i] = array[i - 1] + array[i-2];
+		}
+	}
+	return array[n];
 }
 int main()
 {
+
 	int num;
 	cin >> num;
-	cout << RecursionFibonachi(num) << "\n";
+	cout << IterationFibonachi(num) << "\n";
 	return 0;
 }
